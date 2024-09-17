8be1f43693032b640cbeae84badf97bdbfdd6dde
MCF4N <veal_klutzy0o@icloud.com>
2024-09-17 10:22:12 -0400

    Add files via upload

---
+++ Working_list.py
@@ -0,0 +1,16 @@
+pizza = ["cheese", "bacon", "sauasge"]
+for pizza_name in pizza:
+    print ("I like " + pizza_name)
+
+
+pizza = ["cheese", "bacon", "sauasge"]
+for pizza_name in pizza:
+    print ("I really like " + pizza_name)
+
+    dog = ["family pet" , "friend " , "caring pet" ]
+    for dog_name in dog: 
+        print ("A dog would make a good " + dog_name)
+
+animal = ["cute" , "loving" ,"fun"]
+for animal_name in animal:
+    print (" I love dogs , cats and birds becuase they are " + animal_name)
