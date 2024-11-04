a3352dd09d4402b5adec937a301d86c5f7ff9ffe
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-04 18:40:39 -0500

    Add files via upload

---
+++ 02_variables_and_data_types/2-11. Adding Comments_.py
@@ -0,0 +1,15 @@
+#2-2
+
+message2_2 = ("I hope you have a good day")
+print ("You aswell")
+
+print("I think understanding the was the easiest")
+
+
+#2-3
+first_name = "Chris"
+last_name = "Repti"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+print("I enjoyed doing this one the most")
