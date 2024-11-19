d68a372296c92a19e60455b65c45e1827ccaa492
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:45:02 -0500

    Update 03_02_greetings.py

--- 03_02_greetings.py
+++ 03_02_greetings.py
@@ -1,1 +1,16 @@
+Names = ['trek', 'Nick', 'Adam', "Xander"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
 
+Greet = ['trek', 'Nick', 'Adam', 'Xander']
+message = f"How are you doing today , {Greet[1].title()}."
+print(message)
+
+Greet = ['trek', 'Nick', 'Adam', 'Xander']
+message = f"How are you doing today , {Greet[2].title()}."
+print(message)
+
+Greet = ['trek', 'Nick', 'Adam', 'Xander']
+message = f"How are you doing today , {Greet[3].title()}."
+print(message)
