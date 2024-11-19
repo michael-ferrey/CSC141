18010e1af88969573e56c7b57af09aee75ee82cd
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:49:27 -0500

    Create 06_01_person.py

---
+++ 06_01_person.py
@@ -0,0 +1,6 @@
+person = {'first_Name': 'Herny','last_Name': 'James','age': 18,'city': 'Ohio'}
+
+print("First name:", person['first_Name'])
+print("Last name:", person['last_Name'])
+print("Age:", person['age'])
+print("City:", person['city'])
