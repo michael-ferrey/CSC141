5169dd67d0bf903f6881dda3348f2657d775c88d
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:47:49 -0500

    Update 03_09_dinner_guests.py

--- 03_09_dinner_guests.py
+++ 03_09_dinner_guests.py
@@ -1,1 +1,8 @@
-
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
+print(Names[4])
+print(Names[5])
+print(Names[6])
+len(Names)
