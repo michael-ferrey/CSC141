5725011d6c38faadf06537fb2b9c2a435d849a0b
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:30:10 -0500

    Delete 01_03_pyhton_org.py

--- 01_03_pyhton_org.py
+++
@@ -1,2 +0,0 @@
-# I took a python course iny my high school as well as a coding system that used microsoft something 
-# I understnad that a little more then this python but that is becuase it was a little easier and not as much becuase it was in high school
