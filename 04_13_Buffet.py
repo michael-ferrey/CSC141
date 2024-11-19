f69c2252836b588a1dea641f389cb844cd441e11
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:40:24 -0500

    Create 04_13_Buffet.py

---
+++ 04_13_Buffet.py
@@ -0,0 +1,13 @@
+Buffets = ('Chicken','Fruit', 'Rice', 'Pizza', 'Burgers')
+print(Buffets[0])
+print(Buffets[1])
+print(Buffets[2])
+print(Buffets[3])
+print(Buffets[4])
+for Buffet in Buffets:
+  print(Buffet)
+
+Buffets = ('Chicken','Fruit', 'Rice', 'Pizza', 'Burgers')
+print("\nModified Buffets")
+for Buffet in Buffets:
+  print(Buffet)
