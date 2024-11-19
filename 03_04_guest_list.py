12d2622d4945686b991bfacd9204f9cb9be5c906
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:45:55 -0500

    Update 03_04_guest_list.py

--- 03_04_guest_list.py
+++ 03_04_guest_list.py
@@ -1,1 +1,16 @@
+Names = ['trek', 'Michael Jorddan', 'Donald Trump', "Ethan Ferrey"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
 
+Ride = ['trek', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"I would love for you to come to dinner tonight with me  {Ride[1].title()}."
+print(message)
+
+Ride = ['trek', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"Youre welcome to come to dinner with me {Ride[2].title()}."
+print(message)
+
+Ride = ['trek', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"You want to have a dinner with me {Ride[3].title()}."
+print(message)
