0376200e2e09cb4607568ec0bcacb56e2cc6eb93
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:47:51 -0500

    Update 05_09_no_users.py

--- 05_09_no_users.py
+++ 05_09_no_users.py
@@ -1,1 +1,10 @@
+usernames = [] 
 
+if not usernames: 
+    print("We need to find some users!")
+else:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {username}, thank you for logging in again.")
