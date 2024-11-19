9eca02fd23fd13deb03b5ee5ee645211503476d4
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:38:34 -0500

    Create 04_11_my_pizzas_your_pizzas.py

---
+++ 04_11_my_pizzas_your_pizzas.py
@@ -0,0 +1,15 @@
+Top = ['Bacon', 'Pineapple', 'Plain']
+for Top in Top:
+    print(f"{Top.title()} Pizza is my favoirte!")
+
+
+Friend_Pizza = ['Bacon', 'Pineapple', 'Plain', "Mushroom"]
+for Friend_Pizza in Friend_Pizza:
+    print(f"{Friend_Pizza.title()} Pizza is my favoirte!")
+
+Top= ['Bacon', 'Pineapple', 'Plain', "Mushroom"]
+Friend_Pizza = Top[2:4]
+print("My favorite pizza are:")
+print(Top)
+print("\nMy friend's favotrite pizza are:")
+print(Friend_Pizza)
