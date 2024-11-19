be1a9a587ec6267aaf5debf7718006b1a7769a95
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 14:00:01 -0500

    Create 08_03_shirt.py

---
+++ 08_03_shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    print("The shirt size is " + size + " and the message printed is: '" + message + "'.")
+
+make_shirt("Small", "Python power is cool!")
+
+make_shirt(message="We love Python !", size="Medium")
+
