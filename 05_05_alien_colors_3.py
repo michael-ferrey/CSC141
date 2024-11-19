7f9e312cc1c0788f31ac53a7bc356ba0d900beec
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:47:13 -0500

    Update 05_05_alien_colors_3.py

--- 05_05_alien_colors_3.py
+++ 05_05_alien_colors_3.py
@@ -1,1 +1,33 @@
+alien_color = 'green'
 
+if alien_color == 'green':
+    print("You earned 5 points for shooting the green alien!")
+elif alien_color == 'yellow':
+    print("You earned 10 points for shooting the yellow alien!")
+elif alien_color == 'red':
+    print("You earned 15 points for shooting the red alien!")
+else:
+    print("Unknown alien color entered.")
+
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print("You earned 5 points for shooting the green alien!")
+elif alien_color == 'yellow':
+    print("You earned 10 points for shooting the yellow alien!")
+elif alien_color == 'red':
+    print("You earned 15 points for shooting the red alien!")
+else:
+    print("Unknown alien color entered.")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You earned 5 points for shooting the green alien!")
+elif alien_color == 'yellow':
+    print("You earned 10 points for shooting the yellow alien!")
+elif alien_color == 'red':
+    print("You earned 15 points for shooting the red alien!")
+else:
+    print("Unknown alien color entered.")
