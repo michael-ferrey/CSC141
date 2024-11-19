738bb9113d0b3c996ee3169bbbd86eb27e4f9ca0
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:45:24 -0500

    Update 03_03_your_own_list.py

--- 03_03_your_own_list.py
+++ 03_03_your_own_list.py
@@ -1,1 +1,16 @@
+Names = ['trek', 'Plane', 'Submarine', "Tank"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
 
+Ride = ['trek', 'Plane', 'Submarine', 'Tank']
+message = f"I would love to drive a  {Ride[1].title()}."
+print(message)
+
+Ride = ['trek', 'Plane', 'Submarine', 'Tank']
+message = f"I would love to drive a  {Ride[2].title()}."
+print(message)
+
+Ride = ['trek', 'Plane', 'Submarine', 'Tank']
+message = f"HI would love to drive a {Ride[3].title()}."
+print(message)
