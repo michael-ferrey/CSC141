d5838232023bf29bdb93d3d8e042a386c1b4623c
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:47:32 -0500

    Update 05_07_favorite_fruit.py

--- 05_07_favorite_fruit.py
+++ 05_07_favorite_fruit.py
@@ -1,1 +1,16 @@
+favorite_fruits = ['blueberrys', 'orange', 'mango']
 
+if 'mango' in favorite_fruits:
+    print("You really like mango!")
+
+if 'orange' in favorite_fruits:
+    print("You really like orange!")
+
+if 'blueberry' in favorite_fruits:
+    print("You really like blueberry!")
+
+if 'rasberry' in favorite_fruits:
+    print("You really like rasberry!")
+
+if 'canalope' in favorite_fruits:
+    print("You really like canalope!")
