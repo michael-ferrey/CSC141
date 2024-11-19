cca4e6d3b1594350530b7aa76758450d678a16af
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:51:59 -0500

    Create 06_07_people.py

---
+++ 06_07_people.py
@@ -0,0 +1,19 @@
+Person_1 = {'age': '18' , 'location': 'Reading PennsylvanA', 'Firstname': 'Henry', 'Lastname': 'James'}
+print(f" Person ones name is {Person_1['Firstname']}.")
+print(f" Person ones name is {Person_1['Lastname']}.")
+print(f" Herny James is {Person_1['age']}.")
+print(f" Herny James is {Person_1['location']}.")
+
+
+Person_2 = {'age': '21' , 'location': 'Easton PennsylvanA', 'Firstname': 'Kyle', 'Lastname': 'Dunford'}
+print(f" Person ones name is {Person_2['Firstname']}.")
+print(f" Person ones name is {Person_2['Lastname']}.")
+print(f" Kyle Dunford is {Person_2['age']}.")
+print(f" Kyle Dunford is {Person_2['location']}.")
+
+
+Person_3 = {'age': '16' , 'location': 'Scranton PennsylvanA', 'Firstname': 'Jodran', 'Lastname': 'Finderland'}
+print(f" Person ones name is {Person_3['Firstname']}.")
+print(f" Person ones name is {Person_3['Lastname']}.")
+print(f" Jordan Finderland is {Person_3['age']}.")
+print(f" Jordan Finderland is {Person_3['location']}.")
