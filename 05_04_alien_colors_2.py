3233a733ed48cf9ed960a54e263b29fc2449f187
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:46:49 -0500

    Update 05_04_alien_colors_2.py

--- 05_04_alien_colors_2.py
+++ 05_04_alien_colors_2.py
@@ -1,1 +1,15 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You earned 5 points for shooting the alien!")
+else:
+    print("You earned 10 points for shooting the alien!")
+
+alien_color = 'pink'
+
+if alien_color == 'green':
+    print("You earned 5 points for shooting the alien!")
+else:
+    print("You earned 10 points for shooting the alien!")
+
 
