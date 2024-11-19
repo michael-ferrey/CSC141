6b0f755f158a4e354d34f98b29152599dae695fd
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:46:17 -0500

    Update 05_01_condtiaonal_statments.py

--- 05_01_condtiaonal_statments.py
+++ 05_01_condtiaonal_statments.py
@@ -1,1 +1,31 @@
+Age = '18'
+print("Is your age == '18'? I predict True.")
+print(Age == '18')
+print("\nIs your age == '24'? I predict False.")
+print(Age == '24')
 
+Truck = 'Yes'
+print("Is your truck red == 'Yes'? I predict True.")
+print(Truck == 'Yes')
+print("\nIs your truck black == 'No'? I predict False.")
+print(Truck == 'No')
+
+
+Color = 'Blue'
+print("Is your fav color == 'Blue'? I predict True.")
+print(Age == 'Blue')
+print("\nIs your fav color == 'Red'? I predict False.")
+print(Age == 'Red')
+
+
+School = 'I hate it'
+print("Do you hate school == 'I hate it'? I predict True.")
+print(School == 'I love it')
+print("\nDo you love school== 'I love it'? I predict False.")
+print(School == 'I love it')
+
+Hurngy = 'I am starving'
+print("Are you hunrgy == 'I am starving'? I predict True.")
+print(Age == 'I am starving')
+print("\nAre you hunrgy == 'I am okay'? I predict False.")
+print(Age == 'I am okay')
