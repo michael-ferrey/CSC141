714e5b196e3517891a1c2bc46650fc87aebbb8f3
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 14:00:46 -0500

    Create 08_06_city_names.py

---
+++ 08_06_city_names.py
@@ -0,0 +1,10 @@
+def city_country(city, country):
+    return city + ", " + country
+
+city1 = city_country("Sudan" , "Khartoum ")
+city2 = city_country("Phillippines"  , "Maila ")
+city3 = city_country("Morocco" , "Rabit ")
+
+print(city1)
+print(city2)
+print(city3)
