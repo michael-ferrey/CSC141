53efb52713124780996537ab1e29534d690ea255
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:48:38 -0500

    Update 05_12_styling_if_statements.py

--- 05_12_styling_if_statements.py
+++ 05_12_styling_if_statements.py
@@ -1,1 +1,12 @@
+current_users = ['Bobby', 'Kyle', 'Admin', 'Hailey', 'Jaden']
 
+new_users = ['John', 'Hunter', 'Barry', 'Axel', 'Teddy']
+
+lowercase_current_users = [user.lower() for user in current_users]
+
+
+for new_user in new_users:
+    if new_user.lower() in lowercase_current_users:
+        print(f"Sorry, the username '{new_user}' has already been taken. Please enter a new username.")
+    else:
+        print(f"The username '{new_user}' is available.")
