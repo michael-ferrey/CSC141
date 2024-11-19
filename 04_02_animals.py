163ccc20206eff0f6ae8211948754952c50a941d
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:35:05 -0500

    Create 04_02_animals.py

---
+++ 04_02_animals.py
@@ -0,0 +1,5 @@
+Animals = ['Bunnys', 'Dogs', 'Cats']
+for Animals in Animals:
+    print(f"{Animals.title()} would be a great fluffy pet !")
+    
+print("Any of these 3 animals would be great to own")
