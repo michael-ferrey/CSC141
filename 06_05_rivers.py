81d358348bf88de7e9db88458c246f2ce625b306
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:51:12 -0500

    Create 06_05_rivers.py

---
+++ 06_05_rivers.py
@@ -0,0 +1,6 @@
+Glossary = {'Nile': 'Eptgy', 'Amazon river':'Colombia', 'mississppi river':'Minnesota to Louisian'}
+print(f"Nile river runs throguh {Glossary['Nile']}.")
+
+print(f"Amazon river runs throguh {Glossary['Amazon river']}.")
+
+print(f"Mississppi river runs though {Glossary['mississppi river']}.")
