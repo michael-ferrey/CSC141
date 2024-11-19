c8afc33f4869366193ccb3153d3fd55a1192da45
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 14:01:29 -0500

    Create 08_09_messages.py

---
+++ 08_09_messages.py
@@ -0,0 +1,7 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+text_messages = ["Hello, how is your day going?", "Remeber we have meetings today!","It's so good to see you!","Great job at practice today!","Have a good rest of your day!"]
+
+show_messages(text_messages)
