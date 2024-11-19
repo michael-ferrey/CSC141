2e5192c8c69de2c66c3ec7329529525a8664954e
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:47:42 -0500

    Update 05_08_hello_admin.py

--- 05_08_hello_admin.py
+++ 05_08_hello_admin.py
@@ -1,1 +1,7 @@
+usernames = ['Bobby', 'Kyle', 'Admin', 'Hailey', 'Jaden']
 
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for logging in again.")
