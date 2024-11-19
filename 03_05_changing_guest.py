56ca42df07372a49626a333306d85b808ddf5bfe
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:46:09 -0500

    Update 03_05_changing_guest.py

--- 03_05_changing_guest.py
+++ 03_05_changing_guest.py
@@ -1,1 +1,20 @@
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', "Ethan Ferrey"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
 
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"I would love for you to come to dinner tonight with me  {Name[1].title()}."
+print(message)
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"Youre welcome to come to dinner with me {Name[2].title()}."
+print(message)
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"YDo you want to have dinner with me  {Name[3].title()}."
+print(message)
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey']
+message = f"Since Ethan cant make dinner would you like to come  {Name[0].title()}."
+print(message)
