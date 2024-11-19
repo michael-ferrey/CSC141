140638cb9fcf43347f8cd122cb056e3c66aaddae
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 14:00:33 -0500

    Create 08_05_cities.py

---
+++ 08_05_cities.py
@@ -0,0 +1,6 @@
+def describe_city(city, country="Denmark"):
+    print(city + " is in " + country + ".")
+
+describe_city("Aarhus") 
+describe_city("Boston", "United States")  
+describe_city("Aalborg")  
