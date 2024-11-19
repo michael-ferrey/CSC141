ed5db5ec6d59d0420ebe415e1f98eb7b020941f5
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:46:32 -0500

    Update 03_06_more_guest.py

--- 03_06_more_guest.py
+++ 03_06_more_guest.py
@@ -1,1 +1,39 @@
 
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
+print(Names[4])
+print(Names[5])
+print(Names[6])
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I would love for you to come to dinner tonight with me  {Name[0].title()}."
+print(message)
+
+Name =['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"Youre welcome to come to dinner with me {Name[1].title()}."
+print(message)
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"YDo you want to have dinner with me  {Name[2].title()}."
+print(message)
+
+message = ('I have found a bigger tabel')
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner {Name[4].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner  {Name[5].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner   {Name[6].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner   {Name[7].title()}."
+print(message)
