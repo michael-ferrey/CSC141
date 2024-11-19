df018c05d53107f34db0c7e8d3fae95f32fdbddb
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:41:01 -0500

    Create 04_15_Code_Review.py

---
+++ 04_15_Code_Review.py
@@ -0,0 +1,7 @@
+numbers = list(range(1, 21))
+print(numbers)
+Top = ['Bacon', 'Pineapple', 'Plain']
+for Top in Top:
+    print(f"{Top.title()} Pizza is my favoirte!")
+three_numbers = list(range(3,32,3))
+print(three_numbers)
