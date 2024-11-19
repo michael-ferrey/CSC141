d93f540800d93015e939faa496a201c8f0332aa5
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:54:55 -0500

    Create 07_02_restaurant_seating.py

---
+++ 07_02_restaurant_seating.py
@@ -0,0 +1,6 @@
+Seats = input("How many people are in this dinner group? ")
+Seats = int(Seats)
+if Seats >= 8:
+    print("\nYoull have to wait for a open table!")
+else:
+    print("\nThere is a open tabel if youd follow me this way.")
