89597f025316c1c8dd07940f92b7110ac2789eb8
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:51:43 -0500

    Create 06_06_polling.py

---
+++ 06_06_polling.py
@@ -0,0 +1,6 @@
+favorite_languages = {'number0': 'Spanish', 'number1': 'German','number2': 'English', 'number3': 'Chinese', 'number4': 'french'}
+print(f"James favorite language is {favorite_languages['number0']}.")
+print(f"Hurberts least favorite language is {favorite_languages['number1']}.")
+print(f"Logans most favorite language is {favorite_languages['number2']}.")
+print(f"Gregs most favorite language is {favorite_languages['number3']}.")
+print(f"Wallys favorite language is {favorite_languages['number4']}.")
