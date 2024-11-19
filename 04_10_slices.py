d2aa796adf7c97df1aa1a05d90e7f5eb14f96f27
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:37:47 -0500

    Create 04_10_slices.py

---
+++ 04_10_slices.py
@@ -0,0 +1,9 @@
+players = ['James', 'Bill', 'Cole', 'Zesus', 'Adam']
+print(players[0:3])
+
+
+players = ['James', 'Bill', 'Cole', 'Zesus', 'Adam']
+print(players[2:3])
+
+players = ['James', 'Bill', 'Cole', 'Zesus', 'Adam']
+print(players[2:6])
