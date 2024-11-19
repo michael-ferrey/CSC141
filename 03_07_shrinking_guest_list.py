35b65a56c745985911aa6f7b509ff0de23101121
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:47:18 -0500

    Update 03_07_shrinking_guest_list.py

--- 03_07_shrinking_guest_list.py
+++ 03_07_shrinking_guest_list.py
@@ -1,1 +1,84 @@
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+print(Names[1])
+print(Names[2])
+print(Names[3])
+print(Names[4])
+print(Names[5])
+print(Names[6])
 
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I would love for you to come to dinner tonight with me  {Name[0].title()}."
+print(message)
+
+Name =['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"Youre welcome to come to dinner with me {Name[1].title()}."
+print(message)
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"Do you want to have dinner with me  {Name[2].title()}."
+print(message)
+
+message = ('I have found a bigger tabel')
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner {Name[4].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner  {Name[5].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner   {Name[6].title()}."
+print(message)
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+message = f"I have more room so would you like to come to dinner   {Name[7].title()}."
+print(message)
+
+
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+first_owned = Name.pop(6)
+print(f"The table is smaller then wanted so i cant invite you {first_owned.title()}.")
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+first_owned = Name.pop(2)
+print(f"The table is smaller then wanted so i cant invite you {first_owned.title()}.")
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+first_owned = Name.pop(5)
+print(f"The table is smaller then wanted so i cant invite you {first_owned.title()}.")
+
+Names = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+first_owned = Name.pop(3)
+print(f"The table is smaller then wanted so i cant invite you {first_owned.title()}.")
+
+
+
+Name = ['Jack Barrel', 'Michael Jorddan', 'Donald Trump', 'Ethan Ferrey', 'Jordan Ken', 'Kyle Dander', 'Jullia Fender', "Lia Frano"]
+print(Name)
+Name.remove('Jack Barrel')
+print(Name) 
+ 
+Name.remove('Michael Jorddan')
+print(Name)
+
+Name.remove('Donald Trump')
+print(Name)
+
+Name.remove('Ethan Ferrey')
+print(Name)
+
+Name.remove('Jordan Ken')
+print(Name)
+
+Name.remove('Kyle Dander')
+print(Name)
+
+Name.remove('Jullia Fender')
+print(Name)
+
+Name.remove('Lia Frano')
+print(Name)
