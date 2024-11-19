e2600f7c391269ebc6906e0e0ad56b302205d982
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:48:00 -0500

    Update 03_10_every_function.py

--- 03_10_every_function.py
+++ 03_10_every_function.py
@@ -1,1 +1,32 @@
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+print(Names[0])
+print(Names[1])
+print(Names[2])
+print(Names[3])
 
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+message = f"How are you doing today , {Names[0].title()}."
+print(message)
+
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+message = f"How is your day going , {Names[1].title()}."
+print(message)
+
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+message = f"I havent seen you in a while how are you doing , {Names[2].title()}."
+print(message)
+
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+message = f"Longtime no see how have you been , {Names[3].title()}."
+print(message)
+
+
+print("Who are youre friends and what are their names in alphabetical order")
+
+Names = ['Billy D', 'Nate G', 'Benny G', "Xaiver S"]
+print("Here is the original list:")
+print(Names)
+print("\nHere is the sorted list:")
+print(sorted(Names))
+print("\nHere is the original list again:")
+print(Names)
