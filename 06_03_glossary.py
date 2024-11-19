20011b562e09cae2795d78b26b87ec918c81c99f
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:50:31 -0500

    Create 06_03_glossary.py

---
+++ 06_03_glossary.py
@@ -0,0 +1,4 @@
+glossary = {'Variable': 'Sotrage that is paired with a data that has value.','If statements': 'Allows you to make decisions on conditions.','Lists': 'Sequnece of variables that are under single name.','Data Types': 'Grouping of sets that represent values.','Dicitionaires': 'Data structure that has collection of sets'}
+
+for word, meaning in glossary.items():
+    print(f"{word}: {meaning}\n")
