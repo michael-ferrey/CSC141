5baadace1803f4e19bf2e794b26fb5ec2409973b
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:55:29 -0500

    Create 07_04_pizza_toppings.py

---
+++ 07_04_pizza_toppings.py
@@ -0,0 +1,14 @@
+toppings = []
+
+while True:
+    topping = input("Enter a pizza topping (or type 'quit to finish): ")
+
+    if topping.lower() == 'quit':
+        break
+
+    toppings.append(topping)
+    print("I'll add " + topping + " to your pizza.")
+
+print("\nYour pizza will have the following toppings:")
+for topping in toppings:
+    print(topping)
