63bfe3fa28221d06e86ffe0ee455b96f90aa9598
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 13:52:41 -0500

    Create 06_08_pets.py

---
+++ 06_08_pets.py
@@ -0,0 +1,22 @@
+Person_1 = {'age': '18' , 'location': 'Reading PennsylvanA', 'Firstname': 'Henry', 'Lastname': 'James', 'Pet': 'Fish'}
+print(f" Person ones name is {Person_1['Firstname']}.")
+print(f" Person ones name is {Person_1['Lastname']}.")
+print(f" Herny James is {Person_1['age']}.")
+print(f" Herny James lives {Person_1['location']}.")
+print(f" Herny James owns a pet {Person_1['Pet']}.")
+
+
+Person_2 = {'age': '21' , 'location': 'Easton PennsylvanA', 'Firstname': 'Kyle', 'Lastname': 'Dunford', 'Pet': 'cat'}
+print(f" Person twos name is {Person_2['Firstname']}.")
+print(f" Person twos name is {Person_2['Lastname']}.")
+print(f" Kyle Dunford is {Person_2['age']}.")
+print(f" Kyle Dunford lives {Person_2['location']}.")
+print(f" Kyle Dunford lives with his pet {Person_2['Pet']}.")
+
+
+Person_3 = {'age': '16' , 'location': 'Scranton PennsylvanA', 'Firstname': 'Jodran', 'Lastname': 'Finderland', 'Pet': 'Dog'}
+print(f" Person threes name is {Person_3['Firstname']}.")
+print(f" Person threes name is {Person_3['Lastname']}.")
+print(f" Jordan Finderland is {Person_3['age']}.")
+print(f" Jordan Finderland lives {Person_3['location']}.")
+print(f" Jordan Finderland is the pet owner of a {Person_3['Pet']}.")
