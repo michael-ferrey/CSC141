d434d03e7c68a84297e34fadb624084ec4a17af1
michael-ferrey <veal_klutzy0o@icloud.com>
2024-11-19 10:47:33 -0500

    Update 03_08_seeing_the_world.py

--- 03_08_seeing_the_world.py
+++ 03_08_seeing_the_world.py
@@ -1,1 +1,7 @@
-
+Places = ['Detrioit', 'Antarctica', 'Jamica', 'England']
+print("Here is the original list:")
+print(Places)
+print("\nHere is the sorted list:")
+print(sorted(Places))
+print("\nHere is the original list again:")
+print(Places)
